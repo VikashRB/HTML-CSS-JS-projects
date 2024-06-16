8d41a95437f89007d468f661d0cf13206136fd53
VikashRB <160260320+VikashRB@users.noreply.github.com>
2024-06-16 10:19:47 +0530

    Add files via upload

---
+++ Dad Jokes Generator/index.js
@@ -0,0 +1,9 @@
+const apiKey = "VsfCqMpqjjpJsJjM0+6mng==2yzjZEgbkFWSg0aP";
+const btnEl = document.getElementById("btn");
+
+const options = {
+    method : "GET",
+    headers : {
+        "X-Api-Key": apiKey.
+    }
+};
